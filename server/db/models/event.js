3a42567b29446969918db764499ad2da17c2893d
Desiree Nelson <palebluedot4@gmail.com>
2021-04-03 11:44:22 -0400

    first commit, boilermaker imported, sql models defined

---
+++ server/db/models/event.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Event = db.define('event', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  date: {
+    type: Sequelize.DATEONLY,
+    allowNull: false
+  },
+  category: {
+    type: Sequelize.STRING
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  link: {
+    type: Sequelize.TEXT
+  }
+})
+
+module.exports = Event
